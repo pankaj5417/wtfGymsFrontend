8011cd9b6562679bf16c5f2bb3dd4361e9e8c5cc
pankaj5417 <87115576+pankaj5417@users.noreply.github.com>
2022-08-27 10:16:43 +0530

    added media query

--- src/components/GymType/GymType.jsx
+++ src/components/GymType/GymType.jsx
@@ -4,78 +4,71 @@ import { debounce } from "lodash";
 
 import "./gymType.css";
 export const GymType = () => {
+  const [gymData, setgymData] = useState();
 
-    const [gymData,setgymData]=useState()
+  const handleChange = (e) => {
+    const inputVal = e.target.value;
 
-    const handleChange = (e) => {
-        const inputVal = e.target.value;
     
-        // setCity(inputVal);
-        debounceGymData(inputVal);
-      };
-    //   async function getWeather(cityname) {
-    //     try {
-    //       const res = await axios.get(
-    //         `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&units=metric&appid=1fe85b3ad8fa502e23bf446831171936`
-    //       );
-    //       console.log("weatherRes",res);
-    //       setWeatherData(await res.data);
-    //     } catch (err) {
-    //       console.log(err);
-    //     }
-    //   }
-    
-    //   useEffect(() => {
-    //     weatherData && getWeather2();
-    //   }, [weatherData]);
-    
-      const debounceGymData = debounce((query) => {
-        getGymData2(query);
-      }, 1000);
+    debounceGymData(inputVal);
+  };
+  
+  const debounceGymData = debounce((query) => {
+    getGymData2(query);
+  }, 1000);
 
-    useEffect(()=>{
-   getGymData()
-   getGymPlan()
-    },[])
+  useEffect(() => {
+    getGymData();
+    getGymPlan();
+  }, []);
 
-    async function getGymData(){
-      const res=await fetch("https://devapi.wtfup.me/gym/nearestgym?lat=30.325488815850512&long=78.0042384802231")
-      const data=await res.json()
-      console.log('gym1',data)
-      setgymData(data.data)
-    }
+  async function getGymData() {
+    const res = await fetch(
+      "https://devapi.wtfup.me/gym/nearestgym?lat=30.325488815850512&long=78.0042384802231"
+    );
+    const data = await res.json();
+    console.log("gym1", data);
+    setgymData(data.data);
+  }
 
-    async function getGymData2(gymname){
-        const res=await fetch(`https://devapi.wtfup.me/gym/places?`)
-        const data=await res.json()
-        console.log("gym2",data)
-        setgymData(data.data)
+  async function getGymData2(gymname) {
+    const res = await fetch(`https://devapi.wtfup.me/gym/places?lat=30.325488815850512&long=78.0042384802231&city="New Delhi"`);
+    const data = await res.json();
+    console.log("gym2", data);
+    setgymData(data.data);
+  }
+
+  async function getGymPlan() {
+    const res = await fetch(
+      "https://wtfup.me/gym_details/WTF-The-Fitness-Point-Gym/gym/plan",
+      {
+        method: "POST",
+
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify("GLKdIYAWDS2Q8"),
       }
+    );
+    const data = await res.json();
+    console.log("planData", data);
+  }
 
-    async function getGymPlan(){
-        const res=await fetch("https://wtfup.me/gym_details/WTF-The-Fitness-Point-Gym/gym/plan",{
-            method:"POST",
-            
-            headers:{
-                "Access-Control-Allow-Origin": "*",
-                "Content-Type":"application/json"
-            },
-            body:JSON.stringify("GLKdIYAWDS2Q8")
-            
-        })
-        const data=await res.json()
-        console.log("planData",data)
-        
-      }
-      
   return (
     <>
       <div className="gymTypeContainer">
         <div className="searchContainer">
           <i className="fa fa-search searchIcon fa-lg" aria-hidden="true"></i>
-          <input className="searchGyms" type="text" placeholder="Search gym name here" onChange={handleChange}/>&nbsp;&nbsp;
-          <i class="fa fa-map-marker locationIcon" aria-hidden="true"></i>&nbsp;&nbsp;
-          
+          <input
+            className="searchGyms"
+            type="text"
+            placeholder="Search gym name here"
+            onChange={handleChange}
+          />
+          &nbsp;&nbsp;
+          <i class="fa fa-map-marker locationIcon" aria-hidden="true"></i>
+          &nbsp;&nbsp;
           <button className="clear-button">Clear</button>
         </div>
         <div className="gymTypeBottomContainer">
@@ -88,50 +81,48 @@ export const GymType = () => {
                 aria-hidden="true"
               ></i>
 
-              <input className="locationTextBox" type="text" placeholder="Enter Location" />
-
+              <input
+                className="locationTextBox"
+                type="text"
+                placeholder="Enter Location"
+              />
             </div>
             <h2>Price</h2>
-            <input className="PriceTextBox" type="text" placeholder="MIN"/>&nbsp;&nbsp;
-            <input className="PriceTextBox" type="text" placeholder="MAX"/>
+            <input className="PriceTextBox" type="text" placeholder="MIN" />
+            &nbsp;&nbsp;
+            <input className="PriceTextBox" type="text" placeholder="MAX" />
             <h2>Cities</h2>
             <select className="selectCity" name="" id="">
-                <option value="delhi">Delhi</option>
-                <option value="delhi">Mumbai</option>
-
+              <option value="delhi">Delhi</option>
+              <option value="delhi">Mumbai</option>
             </select>
-
           </div>
           <div className="gymResults">
-            {
-                gymData?.map(data=>(
-                    <>
-                    <div className="gymResultsContainer">
-                        <div className="gymFreeGif">
-
-                            <img className="freeGifImg" src="https://wtfup.me/assets/assets/gif/free.gif" alt="gymFreeGif" />
-
-                        </div>
-                        <div className="gymInfo">
+            {gymData?.map((data) => (
+              <>
+                <div className="gymResultsContainer">
+                  <div className="gymFreeGif">
+                    <img
+                      className="freeGifImg"
+                      src="https://wtfup.me/assets/assets/gif/free.gif"
+                      alt="gymFreeGif"
+                    />
+                  </div>
+                  <div className="gymInfo">
                     <h2>{data.gym_name}</h2>
                     <p>{data.rating}</p>
-                    <p>{data.address1+","+data.address2}</p>
-                    <p>{data.duration_text+""+data.distance_text}</p>
+                    <p style={{fontSize:"23px",}}>{data.address1 + "," + data.address2}</p>
+                    <p><i class="fa fa-paper-plane sendIcon" aria-hidden="true">&nbsp;</i>
+                    {data.duration_text + " away | " + data.distance_text}</p>
 
                     <div className="gymPrice">
-                        <h2>{"3000 for 3 months"}</h2>
-                        <button className="bookNow-button">Book Now</button>
-
+                      <h2>{"₹3000 for 3 months"}</h2>
+                      <button className="bookNow-button">Book Now</button>
                     </div>
-
-                        </div>
-
-                    </div>
-                    </>
-                )
-
-                )
-            }
+                  </div>
+                </div>
+              </>
+            ))}
           </div>
         </div>
       </div>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,21 +3,25 @@ import { Link } from "react-router-dom";
 import { Sidebar } from "../sidebar/Sidebar";
 import "./navbar.css";
 export const Navbar = () => {
-  const [openSidebar,setOpenSidebar]=useState("false")
-  const handlOpenSidebar=()=>{
+  const [openSidebar, setOpenSidebar] = useState("false");
+  const handlOpenSidebar = () => {
     openSidebar ? setOpenSidebar(false) : setOpenSidebar(true);
-  }
+  };
   return (
     <>
-    <Sidebar openSidebar={openSidebar} setOpenSidebar={setOpenSidebar}/>
+      <Sidebar openSidebar={openSidebar} setOpenSidebar={setOpenSidebar} />
       <div className="navbarContainer">
         <div>
           <ul className="navbarLeft">
             <li className="navbarLeft">
-              <i onClick={handlOpenSidebar} class="fa fa-align-left hamBar" aria-hidden="true"></i>
+              <i
+                onClick={handlOpenSidebar}
+                class="fa fa-align-left hamBar"
+                aria-hidden="true"
+              ></i>
 
               <img
-              className="wtf-logo"
+                className="wtf-logo"
                 src="https://wtfup.me/assets/assets/logo.png"
                 alt="wtf"
               />
@@ -25,58 +29,35 @@ export const Navbar = () => {
           </ul>
         </div>
 
-        
-
         <div>
           <ul className="navbarRight">
             <li>
-              <Link
-                to=""
-                style={{ textDecoration: "none", color: "white" }}
-              >
+              <Link to="" style={{ textDecoration: "none", color: "white" }}>
                 <span className="navbarRightText">Fitness</span>{" "}
-                
               </Link>
             </li>
             <li>
-              <Link
-                to=""
-                style={{ textDecoration: "none", color: "white" }}
-              >
+              <Link to="" style={{ textDecoration: "none", color: "white" }}>
                 <span className="navbarRightText">Nutrition</span>{" "}
-                
               </Link>
             </li>
             <li>
-              <Link
-                to=""
-                style={{ textDecoration: "none", color: "white" }}
-              >
+              <Link to="" style={{ textDecoration: "none", color: "white" }}>
                 <span className="navbarRightText">Gyms</span>{" "}
-                
               </Link>
             </li>
             <li>
-              <Link
-                to=""
-                style={{ textDecoration: "none", color: "white" }}
-              >
+              <Link to="" style={{ textDecoration: "none", color: "white" }}>
                 <span className="navbarRightText">Become WTF Partner</span>{" "}
-                
               </Link>
             </li>
             <li>
-              <Link
-                to=""
-                style={{ textDecoration: "none", color: "white" }}
-              >
+              <Link to="" style={{ textDecoration: "none", color: "white" }}>
                 <span className="navbarRightText">About Us</span>{" "}
-                
               </Link>
             </li>
             <div className="loginRegisterButtonContainer">
               <button className="login-button">Login</button>
-              
             </div>
           </ul>
         </div>
@@ -84,4 +65,3 @@ export const Navbar = () => {
     </>
   );
 };
-
